b0fecb2f3a101e53487fed64d0ff6d7727d30226
francisco-duo <francisco.duo2000@gmail.com>
2024-09-04 10:14:24 -0300

    Adicionando o typescript e incrementando automação para compilar ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Olá!")
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+// Routes
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' });
+});
+
+
+// Server start
+app.listen(3000, () => {
+    console.log('Server is running in port 3000...');
+});
